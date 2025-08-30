054151d47781efe5ea254f2161c64171d3f7ac44
Korbin101lee <khillan5223@gmail.com>
2025-08-29 22:36:48 -0700

    fixed sitemap error

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,30 +1,32 @@
-import { MetadataRoute } from 'next';
+import { MetadataRoute } from "next";
+
+export const dynamic = "force-static";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://korbinhillan.com',
+      url: "https://korbinhillan.com",
       lastModified: new Date(),
-      changeFrequency: 'weekly',
+      changeFrequency: "weekly",
       priority: 1,
     },
     {
-      url: 'https://korbinhillan.com/projects',
+      url: "https://korbinhillan.com/projects",
       lastModified: new Date(),
-      changeFrequency: 'weekly',
+      changeFrequency: "weekly",
       priority: 0.9,
     },
     {
-      url: 'https://korbinhillan.com/resume',
+      url: "https://korbinhillan.com/resume",
       lastModified: new Date(),
-      changeFrequency: 'monthly',
+      changeFrequency: "monthly",
       priority: 0.8,
     },
     {
-      url: 'https://korbinhillan.com/blog',
+      url: "https://korbinhillan.com/blog",
       lastModified: new Date(),
-      changeFrequency: 'weekly',
+      changeFrequency: "weekly",
       priority: 0.7,
-    }
+    },
   ];
 }
